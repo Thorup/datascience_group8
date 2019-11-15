9463423482e38985d7173f7207b3988a524ae538
Xrosby <mads.sfg@gmail.com>
2019-11-15 08:06:16 +0100

    lidt rename

---
+++ Frontend/charts/src/barcharts.js
@@ -0,0 +1,15 @@
+const json =  require('../data/countylocations.json');
+
+
+const initBarCharts = function() {
+    console.log("INIT BARCHARTS")
+}
+
+let addBarChart = (set) => {
+    //bar-charts-container
+    
+    
+}
+
+
+export{initBarCharts}
---
+++ Frontend/charts/src/index.js
@@ -0,0 +1,5 @@
+const heatmap = require('./heatmap.js');
+const barcharts = require('./barcharts.js')
+
+barcharts.initBarCharts()
+heatmap.initHeatMap()
