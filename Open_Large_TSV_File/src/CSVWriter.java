34ddf2054f4fa6bb602cdfe34f6ae8bbc0428c9d
Lasse Hunniche Klevang <lasse96-06@hotmail.com>
2019-12-07 15:41:18 +0100

    aw+9rgujq9+ufjq+9rg

--- Open_Large_TSV_File/src/CSVWriter.java
+++ Open_Large_TSV_File/src/CSVWriter.java
@@ -1,132 +1,140 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
-public class CSVWriter {
-    private String outputFilename = "C:\\Users\\Lasse\\Downloads\\arcos_all_washpost.csv";
+public class CSVWriter
+{
+    private String outputFilename = "C:\\Users\\Lasse\\Downloads\\arcos_all_washpost_TRUE.csv";
     private FileWriter fileWriter;
+    private HashMap<String, Boolean> valuesToInclude;
+    private HashMap<String, Boolean> yearsToInclude;
 
-    public CSVWriter() {
-        try {
+    public CSVWriter()
+    {
+        try
+        {
             fileWriter = new FileWriter(outputFilename);
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
+        initHashMaps();
     }
 
-    public void writeFirstLine(String[] dataTypes) {
-        try {
+    private void initHashMaps()
+    {
+        this.valuesToInclude = new HashMap<>();
+        valuesToInclude.put("buyer_state", true);
+        valuesToInclude.put("buyer_county", true);
+        valuesToInclude.put("drug_name", true);
+        valuesToInclude.put("quantity", true);
+        valuesToInclude.put("transaction_date", true);
+        valuesToInclude.put("dosage_unit", true);
+        valuesToInclude.put("dos_str", true);
+        valuesToInclude.put("transaction_code", true);
+        valuesToInclude.put("measure", true);
+
+        this.yearsToInclude = new HashMap<>();
+        yearsToInclude.put("2007", true);
+        yearsToInclude.put("2008", true);
+        yearsToInclude.put("2009", true);
+        yearsToInclude.put("2010", true);
+        yearsToInclude.put("2011", true);
+        yearsToInclude.put("2012", true);
+
+    }
+
+    public void writeFirstLine(String[] dataTypes)
+    {
+        try
+        {
             String csvLine;
-            for (int i = 0; i < dataTypes.length; i++) {
-                String dataKey = dataTypes[i];
-                if (isIncluded(dataKey)) {
-                    if (i == dataTypes.length - 1) {
-                        csvLine = dataKey + "\n";
-                    } else {
-                        csvLine = dataKey + ",";
+            for (int i = 0; i < dataTypes.length; i++)
+            {
+                if (isIncluded(dataTypes[i]))
+                {
+                    if (i == dataTypes.length - 1)
+                    {
+                        csvLine = dataTypes[i] + "\n";
+                    }
+                    else
+                    {
+                        csvLine = dataTypes[i] + ",";
                     }
                     fileWriter.write(csvLine);
                 }
             }
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             ex.printStackTrace();
         }
     }
 
-    public void writeLineToCSVFile(TSVInstance dataInstance) {
-        try {
+    public void writeLineToCSVFile(TSVInstance dataInstance)
+    {
+        try
+        {
             String csvLine = "";
             String[] dataKeyArray = new String[dataInstance.getDataMap().size()];
             dataInstance.getDataMap().keySet().toArray(dataKeyArray);
-            for (int i = 0; i < dataKeyArray.length; i++) {
+            for (int i = 0; i < dataKeyArray.length; i++)
+            {
                 String dataKey = dataKeyArray[i];
-                if (isIncluded(dataKey)) {
+                if (isIncluded(dataKey))
+                {
                     String dataValue = dataInstance.getDataMap().get(dataKey);
-                    if (dataValue.contains(",")) {
-                        dataValue = dataValue.replace(",",";");
+                    if (dataValue.contains(","))
+                    {
+                        dataValue = dataValue.replace(",", ";");
                     }
-                    if (dataValue.startsWith("  ")) {
+                    if (dataValue.startsWith("  "))
+                    {
                         dataValue = dataValue.substring(2);
-                    } else if (dataValue.startsWith(" ")) {
+                    }
+                    else if (dataValue.startsWith(" "))
+                    {
                         dataValue = dataValue.substring(1);
                     }
 
-                    if (i == dataKeyArray.length - 1) {
+                    if (i == dataKeyArray.length - 1)
+                    {
                         csvLine = csvLine + dataValue + "\n";
-                    } else {
+                    }
+                    else
+                    {
                         csvLine = csvLine + dataValue + ",";
                     }
                 }
             }
-            if (isWithinYear(dataInstance.getDataMap().get("TRANSACTION_DATE".toLowerCase()))) {
-                //System.out.println(csvLine);
+            if (isWithinYear(dataInstance.getDataMap().get("transaction_date")))
+            {
                 fileWriter.write(csvLine);
             }
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             ex.printStackTrace();
         }
     }
 
-    public void closeFileWriter() {
-        try {
+    public void closeFileWriter()
+    {
+        try
+        {
             fileWriter.flush();
             fileWriter.close();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
 
-    private boolean isIncluded(String dataKey) {
-        if ("BUYER_DEA_NO".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("BUYER_BUS_ACT".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("BUYER_NAME".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("BUYER_ADDRESS1".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("BUYER_CITY".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("BUYER_STATE".toLowerCase().equals(dataKey)) {
-            return true;
-        } else if ("BUYER_ZIP".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("BUYER_COUNTY".toLowerCase().equals(dataKey)) {
-            return true;
-        } else if ("DRUG_NAME".toLowerCase().equals(dataKey)) {
-            return true;
-        } else if ("QUANTITY".toLowerCase().equals(dataKey)) {
-            return true;
-        } else if ("TRANSACTION_DATE".toLowerCase().equals(dataKey)) {
-            return true;
-        } else if ("DOSAGE_UNIT".toLowerCase().equals(dataKey)) {
-            return true;
-        } else if ("Product_Name".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("Measure".toLowerCase().equals(dataKey)) {
-            return false;
-        } else if ("dos_str".toLowerCase().equals(dataKey)) {
-            return true;
-        } else {
-            return false;
-        }
+    private boolean isIncluded(String dataKey)
+    {
+        return valuesToInclude.getOrDefault(dataKey, false);
     }
 
-    private boolean isWithinYear(String date) {
-        String year = date.substring(date.length()-4);
-        if (year.equals("2007")) {
-            return true;
-        } else if (year.equals("2008")) {
-            return true;
-        } else if (year.equals("2009")) {
-            return true;
-        } else if (year.equals("2010")) {
-            return true;
-        } else if (year.equals("2011")) {
-            return true;
-        } else if (year.equals("2012")) {
-            return true;
-        } else {
-            return false;
-        }
+    private boolean isWithinYear(String date)
+    {
+        return yearsToInclude.getOrDefault(date.substring(date.length() - 4), false);
     }
 }
--- Open_Large_TSV_File/src/ReadTSVFile.java
+++ Open_Large_TSV_File/src/ReadTSVFile.java
@@ -2,52 +2,67 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 
-public class ReadTSVFile {
+public class ReadTSVFile
+{
     private String inputFilename = "C:\\Users\\Lasse\\Downloads\\arcos_all_washpost.tsv";
     private BufferedReader bufferedReader;
     private String[] dataTypes;
 
-    public ReadTSVFile() {
+    public ReadTSVFile()
+    {
         setBufferedReader();
         setDataTypes();
     }
 
-    private void setBufferedReader() {
-        try {
+    private void setBufferedReader()
+    {
+        try
+        {
             bufferedReader = new BufferedReader(new FileReader(inputFilename));
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e)
+        {
             e.printStackTrace();
         }
     }
 
-    private void setDataTypes() {
-        try {
+    private void setDataTypes()
+    {
+        try
+        {
             String line = bufferedReader.readLine();
             dataTypes = line.split("\\t");
 
-            for (int i = 0; i < dataTypes.length; i++) {
+            for (int i = 0; i < dataTypes.length; i++)
+            {
                 dataTypes[i] = dataTypes[i].toLowerCase();
             }
 
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             ex.printStackTrace();
         }
     }
 
-    public void readFileAndWriteToJson(JsonWriter jsonWriter) {
+    public void readFileAndWriteToJson(JsonWriter jsonWriter)
+    {
         String line;
-        try {
+        try
+        {
             TSVInstance dataInstance = new TSVInstance();
             int lineNumber = 0;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (lineNumber > 0) {
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                if (lineNumber > 0)
+                {
                     jsonWriter.prepareWriteNewLineToJsonFile();
                 }
                 String[] dataElements = line.split("\\t");
 
                 dataInstance.resetTSVInstance();
-                for (int i = 0; i < dataElements.length; i++) {
+                for (int i = 0; i < dataElements.length; i++)
+                {
                     dataInstance.addElement(dataTypes[i], dataElements[i]);
                 }
                 dataInstance.setId(lineNumber);
@@ -56,20 +71,27 @@ public class ReadTSVFile {
                 //System.out.println(line);
                 ++lineNumber;
             }
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             ex.printStackTrace();
         }
     }
 
-    public void readFileAndWriteToCSV(CSVWriter csvWriter) {
+    public void readFileAndWriteToCSV(CSVWriter csvWriter)
+    {
+        //long lineCounter = 0;
         int printCounter = 0;
-        int rollover = 100000;
+        int rollover = 1000000;
+        int kCounter = 1;
         String line;
-        try {
+        try
+        {
             TSVInstance dataInstance = new TSVInstance();
             int lineNumber = 0;
             csvWriter.writeFirstLine(dataTypes);
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null)
+            {
+//                lineCounter++;
                 String[] dataElements = line.split("\\t");
                 dataInstance.resetTSVInstance();
                 for (int i = 0; i < dataElements.length; i++) {
@@ -78,8 +100,16 @@ public class ReadTSVFile {
                 dataInstance.setId(lineNumber);
                 csvWriter.writeLineToCSVFile(dataInstance);
                 ++lineNumber;
+                printCounter++;
+                if(printCounter % rollover == 0)
+                {
+                    System.out.println(kCounter + " million rows printed - " + new Date().toString());
+                    kCounter++;
+                    printCounter = 0;
+                }
             }
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             ex.printStackTrace();
         }
     }
