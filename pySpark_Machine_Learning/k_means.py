043df9950311da8492d37a89e9d05ca222e10f12
Johan Jakobsen <johan.jakobsen@hotmail.com>
2019-12-18 11:41:02 +0100

    one step further

--- pySpark_Machine_Learning/k_means.py
+++ pySpark_Machine_Learning/k_means.py
@@ -6,6 +6,7 @@ from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark import SparkConf, SparkContext
 from pyspark.sql.functions import monotonically_increasing_id
 from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import mean as _mean, stddev as _stddev, col
 # $example off$
 
 from matplotlib import pyplot as plt
@@ -31,12 +32,51 @@ dataset = spark.read.csv(hadoopFile, inferSchema = True, header = True)
 
 #dataset.select('Opioid_Factor', 'Crime_Percent', 'Homeless_Percent', 'Average_Income', 'Unemployment_Percent', 'Population').show()
 
-def prepareData():
+def newOpioidFactor():
     df1 = dataset.select(dataset['Opioid_Factor'] / dataset['Population']).withColumnRenamed("(Opioid_factor / Population)", "new_opioid_factor")
     df11 = df1.withColumn("columnindex", monotonically_increasing_id())
     df22 = dataset.withColumn("columnindex", monotonically_increasing_id())
-    new_df = df22.join(df11, df22.columnindex == df11.columnindex, 'inner').drop(df11.columnindex).drop(df22.columnindex)
-    return new_df
+    final_df = df22.join(df11, df22.columnindex == df11.columnindex, 'inner').drop(df11.columnindex).drop(df22.columnindex)
+    final_df.show()
+    return final_df
+
+def incomeZScore():
+    df_stats = dataset.select(
+    _mean(col('Average_Income')).alias('mean'),
+    _stddev(col('Average_Income')).alias('std')).collect()
+
+    mean = df_stats[0]['mean']
+    std = df_stats[0]['std']
+
+    df1 = dataset.select((dataset['Average_Income'] - mean)/std).withColumnRenamed("(Average_Income - mean / std)", "z_score_AvgInc").alias("z_score_AvgInc")
+    df11 = df1.withColumn("columnindex", monotonically_increasing_id())
+    df22 = dataset.withColumn("columnindex", monotonically_increasing_id())
+    final_df = df22.join(df11, df22.columnindex == df11.columnindex, 'inner').drop(df11.columnindex).drop(df22.columnindex)
+    final_df.show()
+    return final_df
+
+
+
+def prepareData():
+    df_newOpiFac = newOpioidFactor()
+    df_newOpiFac.show()
+    df_AvgInc = incomeZScore()
+
+    df_stats = df_newOpiFac.select(
+    _mean(col('new_opioid_factor')).alias('mean'),
+    _stddev(col('new_opioid_factor')).alias('std')).collect()
+
+    mean = df_stats[0]['mean']
+    std = df_stats[0]['std']
+
+    df1 = df_newOpiFac.select((dataset['new_opioid_factor'] - mean)/std).withColumnRenamed("(new_opioid_factor - mean / std)", "z_score_opioid").alias("z_score_opioid")
+    df11 = df1.withColumn("columnindex", monotonically_increasing_id())
+    df22 = df_AvgInc.withColumn("columnindex", monotonically_increasing_id())
+    final_df = df22.join(df11, df22.columnindex == df11.columnindex, 'inner').drop(df11.columnindex).drop(df22.columnindex)
+
+    final_df.show()
+
+    return final_df
 
 def save_csv(df, filename):
     df.select("*").repartition(1).write.format("com.databricks.spark.csv").option('header', 'true').save("/"+filename)
