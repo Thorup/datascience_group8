e1831de09eb0ce881e8d0cc211f763a652cd7a50
Xrosby <mads.sfg@gmail.com>
2019-12-18 11:39:51 +0100

    minor fix

--- Frontend/charts/src/linecharts.js
+++ Frontend/charts/src/linecharts.js
@@ -1,5 +1,7 @@
 import vegaEmbed from 'vega-embed'
-import { reduce } from 'vega-lite/build/src/encoding'
+import {
+    reduce
+} from 'vega-lite/build/src/encoding'
 let countyMonthlyOpioidUse = require("../data/full_sets/LATEST_TRUE_FILE_2.json")
 
 
@@ -16,34 +18,34 @@ let getLineData = () => {
 
     let reducedSet = []
     let combineDate = countyMonthlyOpioidUse
-    .filter(county => county.opioid_factor != undefined)
-    .filter(county => county.year != undefined)
-    .filter(county => county.month != undefined)
-    .filter(county => county.opioid_factor < 2500000)
-    .map(county => {
-        return {
-            date: county.month + " " + county.year,
-            opioid_factor: county.opioid_factor,
-            fips : county.fips
-        }
-    })
+        .filter(county => county.opioid_factor != undefined)
+        .filter(county => county.year != undefined)
+        .filter(county => county.month != undefined)
+        .filter(county => county.opioid_factor < 2500000)
+        .map(county => {
+            return {
+                date: county.month + " " + county.year,
+                opioid_factor: county.opioid_factor,
+                fips: county.fips
+            }
+        })
     let distinctDates = [...new Set(combineDate
-        .map(county => county.date))]
+            .map(county => county.date))]
         .filter(date => date != undefined)
 
 
     console.log(distinctDates)
-        console.log(combineDate)
+    console.log(combineDate)
 
-    for(let i = 0; i < distinctDates.length; i++){
+    for (let i = 0; i < distinctDates.length; i++) {
         let date = distinctDates[i]
-        let opioidSum  = 0;
+        let opioidSum = 0;
 
 
 
         combineDate.forEach(county => {
-            if(county.date == date) {
-                let factor= Math.floor(parseInt(county.opioid_factor))
+            if (county.date == date) {
+                let factor = Math.floor(parseInt(county.opioid_factor))
                 opioidSum = opioidSum + factor
             }
         })
@@ -51,32 +53,42 @@ let getLineData = () => {
             date: date,
             opioid_factor: opioidSum
         })
-      
+
     }
     console.log(reducedSet)
-    
-   return reducedSet
+
+    return reducedSet
 }
 let createLineChart = (data) => {
-   let lineChart =   
-   {
-    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-    "description": "Development in Opioid Purchases By Pharmacies from 2007 to 2012",
-    "title": "Development in Opioid Purchases and Thefts",
-    "width" : 1900,
-    "height": 1000,
-    "data": {values: data},
-    "mark": "line",
-    "encoding": {
-        "x": {"field": "date", "type": "temporal", "timeUnit": "monthyear", "title": "Date"},
-        "y": {"field": "opioid_factor", "type": "quantitative", "title": "Opioid sales and thefts in mg"}
-  }
-}
-    return lineChart
-}
+        let lineChart = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "description": "Development in Opioid Purchases By Pharmacies from 2007 to 2012",
+            "title": "Development in Opioid Purchases and Thefts",
+            "width": 1900,
+            "height": 1000,
+            "data": {
+                values: data
+            },
+            "mark": "line",
+            "encoding": {
+                "x": {
+                    "field": "date",
+                    "type": "temporal",
+                    "timeUnit": "monthyear",
+                    "title": "Date"
+                },
+                "y": {
+                    "field": "opioid_factor",
+                    "type": "quantitative",
+                    "title": "Opioid sales and thefts in mg"
+                }
+            }
+            }
+            return lineChart
+        }
 
 
 
-export {
-    initLineCharts
-}
+        export {
+            initLineCharts
+        }
